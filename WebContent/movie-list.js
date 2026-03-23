7eb1db8c5ef92f1ecd1f004cc11c28b74c2eb524
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-23 15:41:00 -0700

    feat: populate table with top 20 rated movies

--- WebContent/movie-list.js
+++ WebContent/movie-list.js
@@ -1,3 +1,22 @@
+function handleResult(resultData) {
+    let movieTable = jQuery("#movie-table-body");
+
+    resultData.forEach(movie => {
+        let row = `
+            <tr>
+                <td>${movie['title']}</td>
+                <td>${movie['year']}</td>
+                <td>${movie['director']}</td>
+                <td>${movie['genres'].slice(0, 3).join(', ')}</td>
+                <td>${movie['stars'].slice(0, 3).join(', ')}</td>
+                <td>${movie['rating']}</td>
+            </tr>
+        `;
+
+        movieTable.append(row);
+    });
+}
+
 jQuery.ajax({
    dataType: "json",
    method: "GET",
