2a1dd260f44de2ab79a0580f4df3384c22c77f9d
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-23 11:34:04 -0700

    feat: request movie data with AJAX

---
+++ WebContent/single-movie.js
@@ -0,0 +1,27 @@
+function getParameterByName(target) {
+    // Retrieve URL from browser window
+    let url = window.location.href;
+
+    //Escape special characters
+    target = target.replace(/[\[\]]/g, "\\$&");
+
+    // Build and execute regular expression
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)");
+    let results = regex.exec(url);
+
+    // Handle no matches and empty values
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    return decodeURIComponent(results[2]);
+}
+
+let movieId = getParameterByName('id')
+
+// 404 Page for failure?
+jQuery.ajax({
+    dataType: "json",
+    method: "GET",
+    url: "api/single-movie?id=" + movieId,
+    success: (resultData) => handleResult(resultData)
+})
