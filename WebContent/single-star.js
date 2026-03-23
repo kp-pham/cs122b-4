024250e6eb7f49343acdf6764138b8fac853c8b4
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-23 10:21:51 -0700

    feat: request star info from database with AJAX

---
+++ WebContent/single-star.js
@@ -0,0 +1,8 @@
+let starId = getParameterByName('id');
+
+jQuery.ajax({
+    dataType: "json",
+    method: "GET",
+    url: "api/single-star?id=" + starId,
+    success: (resultData) => handleResult(resultData)
+});
