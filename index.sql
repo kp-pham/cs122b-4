46f2c790daca849ae8e653a6154379e514b25d0b
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-23 22:19:14 -0700

    refactor: change order of columns in indexes
    Columns in equality filters come before columns in range filters and
    ordering and grouping.

--- index.sql
+++ index.sql
@@ -2,7 +2,7 @@ CREATE INDEX index_movie_ratings
 ON ratings (rating DESC, movieId);
 
 CREATE INDEX index_movie_genres
-ON genres_in_movies (genreId, movieId);
+ON genres_in_movies (movieId, genreId);
 
 CREATE INDEX index_movie_stars
-ON stars_in_movies (starId, movieId);
+ON stars_in_movies (movieId, starId);
