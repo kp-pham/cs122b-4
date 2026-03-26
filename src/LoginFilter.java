43e58dba2adff100a71cea3891d963dd25631c88
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-26 16:41:38 -0700

    feat: create filter for protected web pages

---
+++ src/LoginFilter.java
@@ -0,0 +1,33 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Objects;
+
+@WebFilter(filterName = "LoginFilter", urlPatterns="/*")
+public class LoginFilter implements Filter {
+    private final ArrayList<String> allowedURIs = new ArrayList<>();
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+
+    }
+}
