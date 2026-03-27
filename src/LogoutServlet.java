c3bad8a660792eddbe655901a8d79dc94f923cae
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-27 15:38:27 -0700

    refactor: remove data source
    No database connection needed to invalidate session

--- src/LogoutServlet.java
+++ src/LogoutServlet.java
@@ -24,16 +24,6 @@ import java.util.Objects;
 public class LogoutServlet extends HttpServlet {
     private static final long serialVersionUID = 2L;
 
-    private DataSource dataSource;
-
-    public void init(ServletConfig config) {
-        try {
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
-        } catch (NamingException e) {
-            e.printStackTrace();
-        }
-    }
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         HttpSession session = request.getSession(false);
 
