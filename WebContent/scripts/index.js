01435c6df9108a89f6c1c39f3d27c843e1ec7851
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-28 09:34:36 -0700

    feat: show prefixes dynamically

--- WebContent/scripts/index.js
+++ WebContent/scripts/index.js
@@ -2,11 +2,22 @@ function handleResult(resultData) {
     let genresList = $("#genres-list");
 
     resultData.forEach((genre, index) => {
-
         let link = `<a href="list.html?genre=${encodeURIComponent(genre)}">${genre}</a>`;
         genresList.append(link);
     });
 }
+
+function showPrefixes() {
+    let prefixList = $("#prefix-list");
+    let prefixes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*";
+
+    for (const prefix of prefixes) {
+        let link = `<a href=:list.html?prefix=${encodeURIComponent(prefix)}>${prefix}</a>`;
+        prefixList.append(link);
+    }
+}
+
+showPrefixes();
 
 jQuery.ajax({
     dataType: "json",
