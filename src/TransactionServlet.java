061741e5ec920e5002ddd550508ca7a5f286c2ef
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-29 11:18:24 -0700

    feat: create servlet for transactions

---
+++ src/TransactionServlet.java
@@ -0,0 +1,39 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@WebServlet(name = "TransactionServlet", urlPatterns = "/api/transactions")
+public class TransactionServlet extends HttpServlet {
+    private static final long serialVersionUID = 2L;
+
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+}
