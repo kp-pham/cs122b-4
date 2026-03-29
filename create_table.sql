3122d908f250dbb9bc235e239f8c99ee97a8327a
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-29 13:57:35 -0700

    refactor: apply changes when creating database

--- create_table.sql
+++ create_table.sql
@@ -75,6 +75,14 @@ CREATE TABLE ratings(
     FOREIGN KEY (movieId) REFERENCES movies(id)
 );
 
+ALTER TABLE movies
+    ADD COLUMN price DECIMAL(10, 2) NOT NULL;
+
+UPDATE movies
+SET price =
+        FLOOR(1 + RAND() * 30) +
+        ELT(FLOOR(1 + RAND() * 3), 0.99, 0.49, 0.00);
+
 CREATE INDEX index_movie_ratings
     ON ratings (rating DESC, movieId);
 
