9f6d1ea212d58d24221198342fc8b2a446637fd3
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-30 22:16:38 -0700

    feat: handle success and failure for payments

--- WebContent/scripts/payment.js
+++ WebContent/scripts/payment.js
@@ -1,5 +1,18 @@
+const paymentForm = $("#payment-form");
+
 function handleResult(resultData) {
     $("#total").text(`Order Total: $${resultData["total"].toFixed(2)}`);
+}
+
+function submitPaymentForm(formSubmitEvent) {
+    jQuery.ajax({
+        dataType: "json",
+        method: "POST",
+        data: paymentForm.serialize(),
+        url: "api/transactions",
+        success: (resultData) => handlePaymentResult(resultData),
+        failure: (resultData) => handlePaymentFailed(resultData)
+    });
 }
 
 jQuery.ajax({
@@ -8,3 +21,5 @@ jQuery.ajax({
     url: "api/transactions",
     success: (resultData) => handleResult(resultData)
 });
+
+paymentForm.submit(submitPaymentForm);
