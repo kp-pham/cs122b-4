799599880bccbf06dcc4f6a21adecd9cddbe451a
Kevin Pham <kevintp5@uci.edu>
2026-04-13 21:10:49 -0700

    feat: change customers table schema

--- src/UpdateSecurePassword.java
+++ src/UpdateSecurePassword.java
@@ -8,5 +8,17 @@ import org.jasypt.util.password.PasswordEncryptor;
 import org.jasypt.util.password.StrongPasswordEncryptor;
 
 public class UpdateSecurePassword {
+    public statis void main(String[] args) throws Exception {
+        String username = System.getenv("USER");
+        String password = System.getenv("PASSWORD");
+        String url = System.getenv("URL");
 
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(username, password, url);
+        Statement statement = conn.createStatement();
+
+        String alterQuery = "ALTER TABLE customers MODIFY COLUMN password VARCHAR(128)";
+        int alterResult = statement.executeUpdate(alterQuery);
+        System.out.println("altering customers table schema completed, " + alterResult + " rows affected");
+    }
 }
