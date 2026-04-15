a4c60fd2d4fdc90aef25bb05993a3b7e6e5d3dac
Kevin Pham <kevintp5@uci.edu>
2026-04-15 08:46:15 -0700

    feat: query schema for column name and data type

--- src/employees/SchemaServlet.java
+++ src/employees/SchemaServlet.java
@@ -16,6 +16,8 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public class SchemaServlet extends HttpServlet {
     private static final long serialVersionUID = 2L;
@@ -27,6 +29,35 @@ public class SchemaServlet extends HttpServlet {
             dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
         } catch (NamingException e) {
             e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+
+        PrintWriter out = response.getWriter();
+
+        try (Connection conn = dataSource.getConnection()) {
+            String query = "SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE " +
+                           "FROM INFORMATION_SCHEMA.COLUMNS " +
+                           "WHERE TABLE_SCHEMA = ? " +
+                           "ORDER BY TABLE_NAME";
+
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, System.getenv("DATABASE"));
+
+            ResultSet rs = statement.executeQuery();
+
+        } catch (Exception e) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            request.getServletContext().log("Error:", e);
+            response.setStatus(500);
+
+        } finally {
+            out.close();
         }
     }
 }
