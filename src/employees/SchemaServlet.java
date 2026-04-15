3c535ad2413043390b16c055929caaf20ec2f822
Kevin Pham <kevintp5@uci.edu>
2026-04-15 08:32:12 -0700

    feat: create servlet for schema
    Show schema in employee dashboard

---
+++ src/employees/SchemaServlet.java
@@ -0,0 +1,32 @@
+package employees;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class SchemaServlet extends HttpServlet {
+    private static final long serialVersionUID = 2L;
+
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+}
