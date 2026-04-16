be5a47611fce7d5eba2dc388d59968bb81517933
Kevin Pham <kevintp5@uci.edu>
2026-04-15 21:26:29 -0700

    feat: create star when star does not exist

--- WebContent/stored-procedure.sql
+++ WebContent/stored-procedure.sql
@@ -8,7 +8,18 @@ CREATE PROCEDURE add_movie (
     IN genre_name VARCHAR(32),
 )
 BEGIN
+    DECLARE star_id STRING;
 
+    IF NOT EXISTS (SELECT 1 FROM stars WHERE name = star_name) THEN
+       SET star_id =
+       INSERT INTO stars (id, name, birthYear) VALUES ( star_name, )
+--     IF star_name IS NOT NULL THEN
+--         IF EXISTS (SELECT 1 FROM stars WHERE name = star_name) THEN
+--
+--         ELSE
+--
+--         END IF;
+--     END IF;
 END
 
 $$
