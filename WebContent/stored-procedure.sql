b2baa5ea20646c512d4ef81fe5b91304cb394ea8
Kevin Pham <kevintp5@uci.edu>
2026-04-15 21:15:43 -0700

    feat: stored procedure for adding movies

---
+++ WebContent/stored-procedure.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE PROCEDURE add_movie (
+    IN title VARCHAR(100),
+    IN year INTEGER,
+    IN director VARCHAR(100),
+    IN star_name VARCHAR(100),
+    IN genre_name VARCHAR(32),
+)
+BEGIN
+
+END
+
+$$
+
+DELIMITER ;
