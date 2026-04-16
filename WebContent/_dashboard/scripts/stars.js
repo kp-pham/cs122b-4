37fafc4fca86db11665e0627da3f4f0b22ac4ebc
Kevin Pham <kevintp5@uci.edu>
2026-04-15 19:10:17 -0700

    feat: show alerts for success and failure

--- WebContent/_dashboard/scripts/stars.js
+++ WebContent/_dashboard/scripts/stars.js
@@ -1,21 +1,31 @@
 const starsForm = $("#stars-form");
+const alertSuccess  = $("#alert-success");
+const alertFailure = $("alert-failure");
 
 const baseURL = window.location.origin + '/' + window.location.pathname.split('/')[1];
 
-function handleSuccess(resultData) {
+function handleSuccess() {
+    alertFailure.addClass("d-none");
 
+    alertSuccess.text("Successfully added star!");
+    alertSuccess.removeClass("d-none");
 }
 
 function handleFailure(jqXHR) {
+    alertSuccess.addClass("d-none");
 
+    alertFailure.text(jqXHR.responseJSON.message ?? "Something went wrong. Please try again.");
+    alertFailure.removeClass("d-none");
 }
 
 function handleFormSubmit(formSubmitEvent) {
     formSubmitEvent.preventDefault();
 
-    jQuery.ajax({
+    $.ajax({
         url: baseURL + "/api/employees/star",
-        success:handleSuccess,
+        method: "POST",
+        data: starsForm.serialize(),
+        success: handleSuccess,
         error: (jqXHR) => handleFailure(jqXHR)
     });
 }
