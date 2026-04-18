799542e207c5867bcc5aeffddf543112d9816941
Kevin Pham <kevintp5@uci.edu>
2026-04-17 21:37:38 -0700

    feat: servlet for full-text search results

---
+++ src/customers/FullTextSearchServlet.java
@@ -0,0 +1,38 @@
+package customers;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@WebServlet(name = "customers.SearchServlet", urlPatterns = "/api/customers/full-text")
+public class FullTextSearchServlet {
+    private static final long serialVersionUID = 2L;
+
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+}
