0137c0f7bc86e96b9d938ab5fccc0312a7f6a51d
Kevin Pham <kevintp5@uci.edu>
2026-04-18 13:49:46 -0700

    feat: read filename from user input

--- src/IngestData.java
+++ src/IngestData.java
@@ -1,13 +1,20 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class IngestData {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("File name: ");
+        String filename = scanner.nextLine();
+
+        parseCSV(filename);
     }
 
-    public static void parseCSV() {
-        try (BufferedReader br = new BufferedReader()) {
+    public static void parseCSV(String filename) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+
 
         } catch (Exception e) {
             e.printStackTrace();
