fd40297ed1813f0969048f045724a0626b755192
Kevin Pham <kevintp5@uci.edu>
2026-04-18 13:43:53 -0700

    feat: BufferedReader to parse CSV files
    Buffered read operations for large files

---
+++ src/IngestData.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class IngestData {
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    public static void parseCSV() {
+        try (BufferedReader br = new BufferedReader()) {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
