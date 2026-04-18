03378f9424de25a1ccecd03e6474ad052a013e0b
Kevin Pham <kevintp5@uci.edu>
2026-04-18 14:07:20 -0700

    feat: create data loader for movies

---
+++ src/loaders/MovieLoader.java
@@ -0,0 +1,11 @@
+package loaders;
+
+import java.io.BufferedReader;
+import java.util.Set;
+import java.util.HashSet;
+
+public class MovieLoader implements DataLoader {
+    public Set<String> load(String file) {
+
+    }
+}
