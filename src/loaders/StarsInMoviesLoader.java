356b6545a7e31693757ffe64fe643f56ecc3536b
Kevin Pham <kevintp5@uci.edu>
2026-04-25 20:53:16 -0700

    feat: load movie star relationships into staging

--- src/loaders/StarsInMoviesLoader.java
+++ src/loaders/StarsInMoviesLoader.java
@@ -12,7 +12,7 @@ public class StarsInMoviesLoader extends DataLoader {
 
     @Override
     protected void createStagingTable() throws SQLException {
-        String dropQuery = "DROP TABLE IF EXISTS genres_staging";
+        String dropQuery = "DROP TABLE IF EXISTS stars_in_movies_staging";
         String createQuery = "CREATE TABLE stars_in_movies_staging( " +
                              "    starId TEXT, " +
                              "    movieId TEXT " +
@@ -29,7 +29,18 @@ public class StarsInMoviesLoader extends DataLoader {
 
     @Override
     protected void loadToStaging(String file) throws SQLException {
+        String query = "LOAD DATA LOCAL INFILE ? " +
+                       "INTO TABLE stars_in_movies_staging " +
+                       "FIELDS TERMINATED BY ',' " +
+                       "ENCLOSED BY '\"' " +
+                       "LINES TERMINATED BY '\\r\\n' " +
+                       "IGNORE 1 ROWS";
 
+        PreparedStatement statement = conn.prepareStatement(query);
+        statement.setString(1, file);
+
+        statement.executeUpdate();
+        statement.close();
     }
 
     @Override
