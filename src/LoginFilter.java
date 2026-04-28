89093ae89d5b484a7e86cb71ad06f0c2f1fc8dc9
Kevin Pham <kevintp5@uci.edu>
2026-04-27 22:04:32 -0700

    refactor: handle when customer or employee only pages are requested

--- src/LoginFilter.java
+++ src/LoginFilter.java
@@ -17,18 +17,32 @@ public class LoginFilter implements Filter {
 
         String requestURI = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
 
-        if (this.isUrlAllowedWithoutLogin(httpRequest.getRequestURI())) {
+        if (this.isUrlAllowedWithoutLogin(requestURI)) {
             chain.doFilter(request, response);
             return;
         }
 
-        if (isEmployeeOnly(httpRequest.getRequestURI()) && httpRequest.getSession().getAttribute("employee") == null) {
-            httpResponse.sendRedirect("_dashboard/login.html");
-        } else if (httpRequest.getSession().getAttribute("customer") == null) {
-            httpResponse.sendRedirect("login.html");
-        } else {
+        if (isEmployeeOnly(requestURI)) {
+            if (httpRequest.getSession().getAttribute("employee") == null) {
+                httpResponse.sendRedirect(httpRequest.getContextPath() + "/_dashboard/login.html");
+                return;
+            }
+
             chain.doFilter(request, response);
+            return;
         }
+
+        if (isCustomerOnly(requestURI)) {
+            if (httpRequest.getSession().getAttribute("customer") == null) {
+                httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.html");
+                return;
+            }
+
+            chain.doFilter(request, response);
+            return;
+        }
+
+        httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.html");
     }
 
     private boolean isUrlAllowedWithoutLogin(String requestURI) {
@@ -36,7 +50,11 @@ public class LoginFilter implements Filter {
     }
 
     private boolean isEmployeeOnly(String requestURI) {
-        return requestURI.startsWith("/cs122b_war/_dashboard") || requestURI.startsWith("/cs122b_war/api/employees");
+        return requestURI.startsWith("/_dashboard") || requestURI.startsWith("/api/employees");
+    }
+
+    private boolean isCustomerOnly(String requestURI) {
+        return requestURI.startsWith("/");
     }
 
     public void init(FilterConfig fConfig) {
