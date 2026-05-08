c885f3824ac3fcc75235620e4a6f1a1113a5ca56
Kevin Pham <kevintp5@uci.edu>
2026-05-08 09:28:34 -0700

    feat: initiate autocomplete search
    Perform search when no search results are cached

--- WebContent/scripts/full-text-search.js
+++ WebContent/scripts/full-text-search.js
@@ -1,8 +1,26 @@
 const fullTextSearch = $("#full-text");
 
+function handleLookup(query, done) {
+    console.log("Autocomplete search initiated: ", query);
+
+    const cached = sessionStorage.getItem(query);
+
+    if (cached) {
+        console.log("");
+
+        const suggestions = JSON.parse(cached);
+
+        console.log("Cached suggestion list: ", suggestions);
+
+        done({ suggestions: suggestions });
+
+        return;
+    }
+}
+
 $("#autocomplete").autocomplete({
     lookup: (query, done) => handleLookup(query, done),
-    onSelect: (suggestion) => handleSelectSuggestion(suggestion),
+    // onSelect: (suggestion) => handleSelectSuggestion(suggestion),
     deferRequestBy: 300,
     minChars: 3,
     triggerSelectOnValidInput: false,
