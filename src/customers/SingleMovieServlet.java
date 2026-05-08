2b89e8d27ca85bb007fe32a8ce7e9251abd323ff
Kevin Pham <kevintp5@uci.edu>
2026-05-08 10:00:52 -0700

    refactor: handle null values when searching for movie
    Mismatch between carriage returns on UNIX and Windows systems

--- src/customers/SingleMovieServlet.java
+++ src/customers/SingleMovieServlet.java
@@ -43,8 +43,19 @@ public class SingleMovieServlet extends HttpServlet {
 
         try (Connection conn = dataSource.getConnection()) {
             String query = "SELECT M.id, M.title, M.year, M.director, R.rating, " +
-                           "CONCAT('[', GROUP_CONCAT(DISTINCT G.name ORDER BY G.name ASC SEPARATOR ', '), ']') AS genres, " +
-                           "CONCAT('[', GROUP_CONCAT(DISTINCT JSON_OBJECT('id', S.id, 'name', S.name) ORDER BY S.movie_count DESC, S.name ASC), ']') AS stars " +
+                           "IFNULL( " +
+                           "    CONCAT('[', " +
+                           "           GROUP_CONCAT(DISTINCT G.name ORDER BY G.name ASC), " +
+                           "           ']'), " +
+                           "    '[]' " +
+                           ") AS genres, " +
+                           "IFNULL( " +
+                           "    CONCAT('[', " +
+                           "           GROUP_CONCAT(DISTINCT CASE WHEN S.id IS NOT NULL THEN JSON_OBJECT('id', S.id, 'name', S.name) END " +
+                           "                        ORDER BY S.movie_count DESC, S.name ASC), " +
+                           "           ']')," +
+                           "    '[]' " +
+                           ") AS stars " +
                            "FROM movies AS M " +
                            "LEFT JOIN genres_in_movies AS GIM ON M.id = GIM.movieId " +
                            "LEFT JOIN genres AS G ON GIM.genreId = G.id " +
