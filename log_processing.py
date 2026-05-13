e79be0e4ddd7e953defa6a17be34bb3f979cec6e
Kevin Pham <kevintp5@uci.edu>
2026-05-12 19:32:54 -0700

    feat: create log processing script
    Calculate average TS and TJ based on number of samples

---
+++ log_processing.py
@@ -0,0 +1,24 @@
+def measure_time():
+    total_ts = 0
+    total_tj = 0
+    samples = 0
+
+    with open("docs/log.txt") as file:
+        for line in file:
+            ts, tj = parse_line(line)
+            total_ts += ts
+            total_tj += tj
+            samples += 1
+
+    average_ts = total_ts / samples
+    average_tj = total_tj / samples
+
+    print(f"Average TS: {average_ts}")
+    print(f"Average TJ: {average_tj}")
+
+
+def parse_line(line):
+    ts, tj = line.split(",")
+    return int(ts[3:]), int(tj[3:])
+
+measure_time()
