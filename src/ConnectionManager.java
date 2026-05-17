a297e3e66d99bf2fc98b0957d8adccef8467bf1e
Kevin Pham <kevintp5@uci.edu>
2026-05-16 20:48:05 -0700

    feat: create data sources for master and slave
    Master handles read/write requests and slave handles only read requests

---
+++ src/ConnectionManager.java
@@ -0,0 +1,13 @@
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class ConnectionManager {
+    public static DataSource getMasterDataSource() throws NamingException {
+        return (DataSource) new InitialContext().lookup("java:comp/env/jdbc/master");
+    }
+
+    public static DataSource getSlaveDataSource() throws NamingException {
+        return (DataSource) new InitialContext().lookup("java:comp/env/jdbc/slave");
+    }
+}
